8daf3b199027067a4fd5c5d6ddbe34a6c9a10d19
him8033 <himanshu8033@gmail.com>
2024-11-22 08:56:09 +0530

    inserting data in sql table

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {faker} = require("@faker-js/faker");
+const { faker } = require("@faker-js/faker");
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
@@ -8,19 +8,26 @@ const connection = mysql.createConnection({
     password: 'Mysql@123'
 });
 
+let q = "INSERT INTO user (id,username,email,password) VALUES ?";
+let users = [
+    ["123", "123_newUser", "newUser@gmail.com", "newUser@123"],
+    ["123a", "123_newUsera", "newUsera@gmail.com", "newUsera@123"],
+    ["123b", "123_newUserb", "newUserb@gmail.com", "newUserb@123"],
+]
+
 try {
-    connection.query("SHOW TABLES",(err,result) =>{
-        if(err) throw err;
+    connection.query(q, [users], (err, result) => {
+        if (err) throw err;
         console.log(result);
     });
-}catch(err){
+} catch (err) {
     console.log(err);
 }
 
 connection.end();
 
 let getRandomUser = () => {
-    return{
+    return {
         id: faker.string.uuid(),
         username: faker.internet.userName(),
         email: faker.internet.email(),
