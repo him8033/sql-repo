b0d8164618e573ed2d4aa7666236c5e6bb2d406a
him8033 <himanshu8033@gmail.com>
2024-11-24 18:55:36 +0530

    update of the username using with patch method

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
+const methodOverride = require("method-override");
 
-app.set("view engine","ejs");
-app.set("views", path.join(__dirname,"/views"));
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -24,43 +27,81 @@ let getRandomUser = () => {
     ]
 }
 
+//      to Show the total no. of users in DataBase
+
 app.get("/", (req, res) => {
     let q = `SELECT COUNT(*) FROM user`;
     try {
         connection.query(q, (err, result) => {
             if (err) throw err;
             let count = result[0]['COUNT(*)'];
-            console.log(count);
-            res.render("home.ejs",{count});
+            res.render("home.ejs", { count });
         });
     } catch (err) {
         console.log(err);
+        console.log("some Error In DataBase");
     }
 })
+
+//      to view all the users in database
 
 app.get("/user", (req, res) => {
     let q = `SELECT * FROM user`;
     try {
         connection.query(q, (err, users) => {
             if (err) throw err;
-            res.render("showUser.ejs",{users});
+            res.render("showUser.ejs", { users });
         });
     } catch (err) {
         console.log(err);
+        console.log("some Error In DataBase");
+    }
+})
+
+//      goto the edit page for editing the username
+
+
+app.get("/user/:id/edit", (req, res) => {
+    let { id } = req.params;
+    let q = `SELECT * FROM user WHERE id='${id}'`;
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+            let user = result[0];
+            res.render("edit.ejs", { user });
+        });
+    } catch (err) {
+        console.log(err);
+        console.log("some Error In DataBase");
+    }
+})
+
+//      Update process through patch method
+
+app.patch("/user/:id", (req, res) => {
+    let { id } = req.params;
+    let { username: newUsername, password: formPass } = req.body;
+    let q = `SELECT * FROM user WHERE id='${id}'`;
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+            let user = result[0];
+            if (formPass != user.password) {
+                res.send("Invalid Password!");
+            }else{
+                q2 = `UPDATE user SET username='${newUsername}' WHERE id='${id}'`;
+                connection.query(q2,(err,result) => {
+                    if (err) throw err;
+                    res.redirect("/user");
+                })
+            }
+        });
+    } catch (err) {
+        console.log(err);
+        console.log("some Error In DataBase");
     }
 })
 
 app.listen(port, (req, res) => {
     console.log("listning on port: ", port);
 })
-
-// try {
-//     connection.query(q, [data], (err, result) => {
-//         if (err) throw err;
-//         console.log(result);
-//     });
-// } catch (err) {
-//     console.log(err);
-// }
-
-// connection.end();
